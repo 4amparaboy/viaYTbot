36c2054e7c5567cf0f21fe89ef6145c83b442dc0
President Tuychiyev | Developer <77186584+presidenttuychiyev@users.noreply.github.com>
2021-01-09 22:56:27 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,14 +1,23 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
-
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
+    # write id and usernames
+    with open('id_tg.txt', 'r') as f1:
+        idlar = f1.readlines()
+
+    if f'{message.chat.id}\n' not in idlar:
+        with open('id_tg.txt', 'a') as f2:
+            f2.write(f'{message.chat.id}\n')
+        with open('users.txt', 'a') as f3:
+            f3.write(f'{message.chat.username}\n')
+
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Bot news 🤩", url="https://t.me/DGUuz")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/president_tuychiyev")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>❕\n♻️ Welcome to <b>@viaYTbot</b>\n\n👉🏼 <a href='https://telegra.ph/More-info-01-09'>for More info</a> 👈🏼"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
